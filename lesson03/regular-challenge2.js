12a914703a7273f0ee5d3ad2309a3dc715bd3014
Waruggy <cwang8413@yahoo.com>
2021-06-01 13:26:44 -0700

    Made a sentence with five words and period

---
+++ lesson03/regular-challenge2.js
@@ -0,0 +1,10 @@
+let str = "The Alliance is are enemy ."
+let arr = str.split(' ');
+
+console.log(arr);
+console.log(arr.splice(2,1));
+
+// [object Array] (5)
+["The","Alliance","is","are","enemy","."]
+// [object Array] (1)
+["is"]
