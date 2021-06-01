edc417e3f8a83c8cf7458c8b6bae51caa38245e2
Waruggy <cwang8413@yahoo.com>
2021-06-01 12:29:26 -0700

    Updated files

---
+++ lesson03/regular-challenge1.js
@@ -0,0 +1,16 @@
+let arr = ['orc', 'troll', 'tauren', 'undead', 'blood elf'];
+
+arr.push('goblin');
+console.log(arr);
+
+arr.splice(2,1);
+console.log(arr);
+
+let str = arr.join(' ');
+console.log(str);
+
+// [object Array] (6)
+["orc","troll","tauren","undead","blood elf","goblin"]
+// [object Array] (5)
+["orc","troll","undead","blood elf","goblin"]
+"orc troll undead blood elf goblin"
