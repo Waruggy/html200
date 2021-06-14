3364ac2462751be889559becbebed57f86fbe7d5
Waruggy <48106241+Waruggy@users.noreply.github.com>
2021-06-14 14:17:32 -0700

    Update regular-challenge1.js

--- lesson03/regular-challenge1.js
+++ lesson03/regular-challenge1.js
@@ -6,11 +6,11 @@ console.log(arr);
 arr.splice(2,1);
 console.log(arr);
 
-let str = arr.join(' ');
+let str = arr.join(', ');
 console.log(str);
 
 // [object Array] (6)
 ["orc","troll","tauren","undead","blood elf","goblin"]
 // [object Array] (5)
 ["orc","troll","undead","blood elf","goblin"]
-"orc troll undead blood elf goblin"
+"orc, troll, undead, blood elf, goblin"
